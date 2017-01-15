629d61f2a6764f7390fa269a583c3aadb35f47cc
papayabird <universe@zokas.com>
2017-01-15 11:02:35 +0800

    add UIObject

---
+++ OriginalPoint/UIObject/OPNavBGView.swift
@@ -0,0 +1,18 @@
+//
+//  OPNavBGView.swift
+//  OriginalPoint
+//
+//  Created by papayabird on 2017/1/15.
+//  Copyright © 2017年 OP. All rights reserved.
+//
+
+import UIKit
+
+class OPNavBGView: UIView {
+
+    override func layoutSubviews() {
+        
+        self.backgroundColor = UIColor.init(colorLiteralRed: 50/255.0, green: 174/255.0, blue: 101/255.0, alpha: 1);
+    }
+
+}
---
+++ OriginalPoint/UIObject/OPTitileLabel.swift
@@ -0,0 +1,19 @@
+//
+//  OPTitileLabel.swift
+//  OriginalPoint
+//
+//  Created by papayabird on 2017/1/15.
+//  Copyright © 2017年 OP. All rights reserved.
+//
+
+import UIKit
+
+class OPTitileLabel: UILabel {
+
+    override func layoutSubviews() {
+        
+        self.font = UIFont.boldSystemFont(ofSize: 18);
+        self.textColor = UIColor.white;
+    }
+    
+}
